0e97331072a2410a3c5630b8c8cc2d73c5f16b14
Cat Harsis <101965352+TACHarsis@users.noreply.github.com>
2022-11-09 07:04:29 +0100

    Tabs to spaces

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -21,19 +21,19 @@ class CfgMods {
         author = "Your Name";
 
 
-        picture 	= "title_co.paa"; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
-        hideName	= 0;	// Hide the extension name
-        hidePicture	= 0;	// Hide the extension menu
+        picture         = "title_co.paa";     // Picture displayed from the expansions menu. Optimal size is 2048x1024
+        hideName        = 0;    // Hide the extension name
+        hidePicture     = 0;    // Hide the extension menu
 
-        logoSmall	 = "icon_64_ca.paa";	// Display next to the item added by the mod 64x64
-        logo		 = "icon_128_ca.paa";			// Logo displayed in the main menu 128x128
-        logoOver	 = "icon_128_highlight_ca.paa";		// When the mouse is over, in the main menu 128x128
-        tooltip      = "Tip of the morning";
-        tooltipOwned = "Who sold this to you?";								// Tool tip displayed when the mouse is left over, in the main menu
-        overview 	 = "Best Mod ever!"; // Overview text, displayed from the extension menu
-        description  = "It's unclear where this will show"; //??
-        action		 = "https://www.ehre.to";			// Website URL, that can accessed from the expansions menu 
-        actionName 	 = "today?"; // label of button/tooltip in extension menu
+        logoSmall       = "icon_64_ca.paa";    // Display next to the item added by the mod 64x64
+        logo            = "icon_128_ca.paa";            // Logo displayed in the main menu 128x128
+        logoOver        = "icon_128_highlight_ca.paa";        // When the mouse is over, in the main menu 128x128
+        tooltip         = "Tip of the morning";
+        tooltipOwned    = "Who sold this to you?";                                // Tool tip displayed when the mouse is left over, in the main menu
+        overview        = "Best Mod ever!"; // Overview text, displayed from the extension menu
+        description     = "It's unclear where this will show"; //??
+        action          = "https://www.ehre.to";            // Website URL, that can accessed from the expansions menu 
+        actionName      = "today?"; // label of button/tooltip in extension menu
 
         // Color used for DLC stripes and backgrounds (RGBA)
         dlcColor[] =
